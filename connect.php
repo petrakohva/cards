0e2f2141d768ed843a43b5afc706c149e8c57683
petrakohva <petra.kohva@haaga-helia.fi>
2019-09-18 10:55:20 +0300

    added a feature for connecting to database

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+function connect_db() {
+ $servername = "localhost";
+ $username = "petra";
+ $password = "password";
+ $dbname = "petra";
+ // Create connection
+GIT Exercises 4 (13)
+29.12.2018
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ // Check connection
+ if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+ } else {
+ }
+ return $conn;
+}
+?>
